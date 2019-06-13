a2234b95c334d2f62f5b2917f8b4e21e843f35e4
lliei0x <git@liei.cc>
2019-06-13 18:16:41 +0800

    feat: 添加数据库 建立数据库模型 && 初始化数据库数据

---
+++ config/config.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+func init() {
+	viper.SetConfigName("config")
+	viper.AddConfigPath("./")
+	viper.SetConfigType("yaml")
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatalf("Fatal error config file: %s \n", err)
+	}
+}
+
+func getPostgreConfg() string {
+	var (
+		dbName   string
+		port     string
+		user     string
+		sslMode  string
+		password string
+		host     string
+	)
+
+	dbName = viper.GetString("db.dbname")
+	port = viper.GetString("db.port")
+	user = viper.GetString("db.user")
+	sslMode = viper.GetString("db.sslmode")
+	password = viper.GetString("db.password")
+	host = viper.GetString("db.host")
+
+	return fmt.Sprintf("host=%s user=%s dbname=%s port=%s sslmode=%s password=%s", host, user, dbName, port, sslMode, password)
+}
