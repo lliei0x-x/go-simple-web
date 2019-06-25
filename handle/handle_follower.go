ee59c92e2da675b83774140959515a40c78e1c21
李磊 <1369183590@qq.com>
2019-06-25 18:13:24 +0800

    feat: improve profile add follow & unfollow 完善用户关注功能

--- handle/handle_follower.go
+++ handle/handle_follower.go
@@ -1,10 +1,37 @@
 package handle
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+
+	"go-simple-web/model"
+
+	"github.com/gorilla/mux"
 )
 
 func followHandler(w http.ResponseWriter, r *http.Request) {
-	usrnamegetSessionUser(r)
+	vars := mux.Vars(r)
+	other := vars["username"]
+	username, _ := getSessionUser(r)
+	cUser, _ := model.GetUserByUsername(username)
+	if err := cUser.Follow(other); err != nil {
+		log.Println("Follow error:", err)
+		w.Write([]byte("Error in Follow"))
+		return
+	}
+	http.Redirect(w, r, fmt.Sprintf("/user/%v", other), http.StatusSeeOther)
 }
-func unFollowHandler(w http.ResponseWriter, r *http.Request) {}
+
+func unFollowHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	other := vars["username"]
+	username, _ := getSessionUser(r)
+	cUser, _ := model.GetUserByUsername(username)
+	if err := cUser.UnFollow(other); err != nil {
+		log.Println("UnFollow error:", err)
+		w.Write([]byte("Error in UnFollow"))
+		return
+	}
+	http.Redirect(w, r, fmt.Sprintf("/user/%v", other), http.StatusSeeOther)
+}
